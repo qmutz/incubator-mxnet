7c9b8d7f27721d46859fbed4830c2066e3798a9b
Iblis Lin <iblis@hs.ntnu.edu.tw>
2019-01-20 10:01:21 +0800

    Julia: add windows-cpu build

---
+++ ci/windows/test_jl07_cpu.ps1
@@ -0,0 +1,33 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+7z x -y windows_package.7z
+
+$env:MXNET_HOME=[io.path]::combine($PSScriptRoot, 'mxnet_home')
+$env:JULIA_URL="https://julialang-s3.julialang.org/bin/winnt/x64/0.7/julia-0.7.0-win64.exe"
+$env:JULIA_DEPOT_PATH=[io.path]::combine($PSScriptRoot, 'julia-depot')
+
+# Download most recent Julia Windows binary
+(new-object net.webclient).DownloadFile($env:JULIA_URL, "C:\julia07\julia-binary.exe")
+
+# Run installer silently, output to C:\julia07\julia
+C:\julia07\julia-binary.exe /S /D=C:\julia07\julia
+
+C:\julia07\julia\bin\julia -e "using InteractiveUtils; versioninfo()"
+C:\julia07\julia\bin\julia -e "using Pkg; Pkg.develop(PackageSpec(name = \"MXNet\", path = \"julia\"))"
+C:\julia07\julia\bin\julia -e "using Pkg; Pkg.build(\"MXNet\"))"
+C:\julia07\julia\bin\julia -e "using Pkg; Pkg.test(\"MXNet\"))"
